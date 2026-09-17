be6ddb730bfb838816250c1a10f790fe56d0c921
Harsh20064 <harshsinghania37@gmail.com>
2026-09-17 14:39:41 +0530

    added no of nodes hw

---
+++ Linked List/Homework/no_of_nodes_cll.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std; 
+
+struct Node{
+    int data;
+    Node *next;
+
+};
+void Traversal(Node *head){
+    if(head != nullptr){
+        Node *temp = head;
+        do{
+            cout << temp -> data << " ";
+            temp = temp -> next;
+        }
+        while(temp != head);
+    }
+        else{
+            cout << "Linked List is empty";
+        }
+        cout << endl;
+}
+int No_of_nodes(Node *head){
+    int ctr = 0;
+    if(head != nullptr){
+        Node *temp = head;
+        do{
+            temp = temp -> next;
+            ctr++;
+        }
+        while(temp != head);
+    }
+        else{
+            cout << "Linked List is empty";
+        }
+        return ctr;
+}
+Node* Creation(Node *head,int n){
+    for(int i = 0;i<n;i++){
+        int value;
+        cout << "Enter the value of Nodes: ";
+        cin >> value;
+        Node * N = new Node;
+        N -> data = value;
+
+        if(head == nullptr){
+            head = N;
+            N -> next = head;
+        }
+        else{
+            Node *temp = head;
+            while(temp -> next != head){
+               temp = temp -> next;
+            }
+            temp -> next = N;
+            N -> next = head;
+        }
+    }
+    return head;
+}
+
+int main(){
+    int n;
+    cout << "Enter number of nodes: ";
+    cin >> n;
+    Node *head = nullptr;
+    head = Creation(head,n);
+    cout << "Original Circular Linked List: ";
+    Traversal(head);
+    int Nodes = No_of_nodes(head);
+    cout  << "No of nodes = " << Nodes;
+    return 0;
+}
+
